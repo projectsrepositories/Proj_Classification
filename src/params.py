ee2895831feb5f6fb62c621765ffc95db2ca371a
Sarita <limbusarita022@gmail.com>
2023-06-27 15:09:53 -0400

    Source changed.

--- src/params.py
+++ src/params.py
@@ -1,4 +1,4 @@
-filename_ip_data = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/ChurnData.csv"
+filename_ip_data = "../inputfile/ChurnData.csv"
 X_columns = ['tenure', 'age', 'address', 'income', 'ed', 'employ', 'equip']
 y_column = 'churn'
 labels = ['No Churn', 'Churn']
